9ac4675f1587a24e4281c222c8b09be5b207e7c6
AnnaelleMyriam <annaelle.benlamri@epfl.ch>
2024-11-14 23:34:49 +0100

    Update results.ipynb and utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -7,8 +7,7 @@ def categorize_release_season(date):
     """
     Categorize a release date into one of the following categories: Spring, Summer, Holiday, or Other.
     Args:
-        date (datetime): The release date of a movie.
-        
+        date (datetime): The release date of a movie.      
     Returns:
         str: The category of the release date.
     """
@@ -24,65 +23,86 @@ def categorize_release_season(date):
         return 'NaN'
 
 def extract_info(infos_str):
-    """Extracts country names from the 'Movie countries (Freebase ID:name tuples)' column."""
+    """
+    Extracts country names from the 'Movie countries (Freebase ID:name tuples)' column.
+    Args:
+        infos_str (str): The string containing the country information.
+    Returns:
+        list: A list of country names.    
+    """
     try:
         return [info.split(":")[1].strip(" }") for info in infos_str.split(",")]
     except Exception:
         return []
 
-# Extract genres and count occurrences within each interval
-def top_n_by_interval(feature,data,n=10):
+def prepare_top_n_data(feature, data, n=10):
+    """
+    Prepares the data by selecting the top n features and ordering them.
+    Args:
+        feature (str): The feature to be analyzed.
+        data (DataFrame): The data to be analyzed.
+        n (int): The number of top features to be considered.
+    Returns:
+        DataFrame: A filtered DataFrame containing only the top n features.
+    """
     df = data.copy()
-    top_n = df.value_counts(feature).head(n)
-    print("Top_n", top_n)
+    top_n = df[feature].value_counts().head(n)
     top_order = top_n.index.tolist()
     
     df = df[df[feature].isin(top_order)]
-    
     df[feature] = pd.Categorical(df[feature], categories=top_order, ordered=True)
     
-    df = df.sort_values(feature)
+    return df
+
+
+def top_n_by_interval(feature, data, n=10, revenue=False):
+    """
+    Plots the top n features by 20-year intervals, either by count or by average revenue.
+    Args:
+        feature (str): The feature to be analyzed.
+        df (DataFrame): The prepared DataFrame with top n features.
+        n (int): The number of top features to be considered.
+        revenue (bool): Whether to calculate by revenue or count.
+    Returns:
+        ax: The plot of the top n features by 20-year intervals.
+    """
+    df = prepare_top_n_data(feature, data, n)
+    if revenue:
+        counts_by_interval = df.groupby(['Year_Interval', feature])['Movie_box_office_revenue'].mean().unstack(fill_value=0)
+    else:
+        counts_by_interval = df.groupby(['Year_Interval', feature]).size().unstack(fill_value=0)
     
-    counts_by_interval = df.groupby(['Year_Interval', feature],observed=False).size().unstack(fill_value=0)
-    
-    # Create a DataFrame for the top genres in each interval
     top_by_interval = counts_by_interval.apply(lambda x: x.nlargest(n), axis=1)
-    
     plot_data = top_by_interval.div(top_by_interval.sum(axis=1), axis=0) * 100
-    
+
     ax = plot_data.plot(kind='bar', stacked=True, figsize=(8, 5), colormap='tab10')
     ax.set_xlabel('Year Interval')
     ax.set_ylabel(f'Percentage of {feature}')
-    ax.set_title(f'Top {n} {feature} by 20-Year Intervals (1910-2016)')
+    title_revenue = 'Average Box Office Revenue' if revenue else 'Occurrences'
+    ax.set_title(f'Top {n} {feature} by {title_revenue} by 20-Year Intervals (1910-2016)')
     ax.legend(title=f'{feature}', bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.tight_layout()
-
+    
     return ax
 
-# Extract genres and count average box office revenue within each interval
-def top_n_by_interval_revenue(feature,data,n=10):
-    df = data.copy()
-    top_n = df.value_counts(feature).head(n)
-    
-    top_order = top_n.index.tolist()
-    
-    df = df[df[feature].isin(top_order)]
-    
-    df[feature] = pd.Categorical(df[feature], categories=top_order, ordered=True)
-    
-    df = df.sort_values(feature)
-    
-    counts_by_interval = df.groupby(['Year_Interval', feature],observed=False)['Movie_box_office_revenue'].mean().unstack(fill_value=0)
-    print("Counts by interval", counts_by_interval)
-    top_by_interval = counts_by_interval.apply(lambda x: x.nlargest(n), axis=1)
-    
-    plot_data = top_by_interval.div(top_by_interval.sum(axis=1), axis=0) * 100
-    
-    ax = plot_data.plot(kind='bar', stacked=True, figsize=(8, 5), colormap='tab10')
-    ax.set_xlabel('Year Interval')
-    ax.set_ylabel(f'Percentage of {feature}')
-    ax.set_title(f'Top {n} {feature} by 20-Year Intervals (1910-2016)')
-    ax.legend(title=f'{feature}', bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.tight_layout()
 
+def top_n_total_revenue(feature, data, n=10):
+    """
+    Plots the top n features by average box office revenue for the entire period.
+    Args:
+        feature (str): The feature to be analyzed.
+        df (DataFrame): The prepared DataFrame with top n features.
+        n (int): The number of top features to be considered.
+    Returns:
+        ax: The plot of the top n features by average box office revenue.
+    """
+    df = prepare_top_n_data(feature, data, n)
+    avg_revenue_by_genre = df.groupby(feature)['Movie_box_office_revenue'].mean().nlargest(n)
+    
+    ax = avg_revenue_by_genre.plot(kind='bar', figsize=(8, 5), colormap='tab10')
+    ax.set_xlabel(feature)
+    ax.set_ylabel('Average Box Office Revenue')
+    ax.set_title(f'Top {n} {feature} by Average Box Office Revenue (1910-2016)')
+    plt.tight_layout()
+    
     return ax
