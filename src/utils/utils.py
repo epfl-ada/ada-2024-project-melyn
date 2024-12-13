df0cca7abbb6858c8ac03462848e743ea0b32d58
najabba <najmeddine.abbassi@epfl.ch>
2024-12-13 02:06:38 +0100

    ReProcessed data and begin modeling

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
+pickle_folder = "../data/pickle/"
+
 def categorize_release_season(date):
     """
     Categorize a release date into one of the following categories: Spring, Summer, Holiday, or Other.
@@ -23,30 +25,20 @@ def categorize_release_season(date):
     else:
         return 'NaN'
 
-def extract_info(infos_str):
-    """
-    Extracts country names from the 'Movie countries (Freebase ID:name tuples)' column.
-    Args:
-        infos_str (str): The string containing the country information.
-    Returns:
-        list: A list of country names.    
-    """
-    try:
-        return [info.split(":")[1].strip(" }") for info in infos_str.split(",")]
-    except Exception:
-        return []
+def clean_columns(feature,top_size,prefix=None):
+    with open(pickle_folder + f'movies_{feature.lower()}_exploded.p', 'rb') as f:
+        df = pickle.load(f)
+    top = df.value_counts(feature).head(top_size).index.to_list()
+    df.loc[~df[feature].isin(top),feature] = "Other"
+    dummies = pd.get_dummies(df[feature],prefix=prefix)
+    df = pd.concat([df.drop(columns=feature),dummies],axis=1
+                    ).groupby('Wikipedia_movie_ID', as_index=False).max()
+    if feature=="Languages":
+        df.columns = [x.lower().replace(" language","") if x.lower().endswith("language") 
+                      else x.lower() if x.lower().startswith("lang_") else x 
+                      for x in df.columns]
+    return df
 
-def exploded_format(feature,df,path):
-    """
-    Save an exploded version of the dataframe of the desired feature 
-    Args:
-        feature (str): The feature to be analyzed.
-        df (DataFrame): The prepared DataFrame
-    """
-    df_exploded = df.explode(feature)[['Wikipedia_movie_ID','Movie_name','Movie_box_office_revenue','Year','Year_Interval',feature]]
-    pickle.dump( df_exploded, open(path,"wb") )
-    display(df_exploded.value_counts(feature))
-    
 def dropempty(df,feature):
     """
     Drop all the rows containg empty list for the column 'feature'
@@ -61,6 +53,30 @@ def dropempty(df,feature):
     df = df[df[feature].apply(lambda x: len(x) > 0)]
     len_after = len(df[feature])
     return df, len_before-len_after
+
+def exploded_format(feature,df,path):
+    """
+    Save an exploded version of the dataframe of the desired feature 
+    Args:
+        feature (str): The feature to be analyzed.
+        df (DataFrame): The prepared DataFrame
+    """
+    df_exploded = df.explode(feature)[['Wikipedia_movie_ID','Movie_name','Movie_box_office_revenue','Year','Year_Interval',f'nb_of_{feature}',feature]]
+    pickle.dump( df_exploded, open(path,"wb") )
+    display(df_exploded.value_counts(feature))
+
+def extract_info(infos_str):
+    """
+    Extracts country names from the 'Movie countries (Freebase ID:name tuples)' column.
+    Args:
+        infos_str (str): The string containing the country information.
+    Returns:
+        list: A list of country names.    
+    """
+    try:
+        return [info.split(":")[1].strip(' }"') for info in infos_str.split(",")]
+    except Exception:
+        return []
 
 def prepare_top_n_data(feature, data, n=10):
     """
@@ -81,6 +97,26 @@ def prepare_top_n_data(feature, data, n=10):
     
     return df
 
+def top_n_average_rating(feature, data, n=10):
+    """
+    Plots the top n features by average rating for the entire period.
+    Args:
+        feature (str): The feature to be analyzed.
+        df (DataFrame): The prepared DataFrame with top n features.
+        n (int): The number of top features to be considered.
+    Returns:
+        ax: The plot of the top n features by average rating.
+    """
+    df = prepare_top_n_data(feature, data, n)
+    avg_revenue_by_genre = df.groupby(feature,observed=False)['averageRating'].mean().nlargest(n)
+    
+    ax = avg_revenue_by_genre.plot(kind='bar', figsize=(8, 5), colormap='tab10')
+    ax.set_xlabel(feature)
+    ax.set_ylabel('Average IMDb Rating')
+    ax.set_title(f'Top {n} {feature} by Average IMDb Rating (1915-2015)')
+    plt.tight_layout()
+    
+    return ax
 
 def top_n_by_interval(feature, data, n=10, revenue=False):
     """
@@ -132,24 +168,4 @@ def top_n_total_revenue(feature, data, n=10):
     plt.tight_layout()
     
     return ax
-
-def top_n_average_rating(feature, data, n=10):
-    """
-    Plots the top n features by average rating for the entire period.
-    Args:
-        feature (str): The feature to be analyzed.
-        df (DataFrame): The prepared DataFrame with top n features.
-        n (int): The number of top features to be considered.
-    Returns:
-        ax: The plot of the top n features by average rating.
-    """
-    df = prepare_top_n_data(feature, data, n)
-    avg_revenue_by_genre = df.groupby(feature,observed=False)['averageRating'].mean().nlargest(n)
     
-    ax = avg_revenue_by_genre.plot(kind='bar', figsize=(8, 5), colormap='tab10')
-    ax.set_xlabel(feature)
-    ax.set_ylabel('Average IMDb Rating')
-    ax.set_title(f'Top {n} {feature} by Average IMDb Rating (1915-2015)')
-    plt.tight_layout()
-    
-    return ax
