ae89579bdf65a1ca623834ef16f0966c5593b730
Lucas <lucas.simonnet@epfl.ch>
2024-12-17 10:10:33 +0100

    website add

---
+++ webSite-Evolution of Cinema/.scala-build/webSite-Evolution of Cinema_f4dd477a3a/src_generated/main/webSite-Evolution%20of%20Cinema.worksheet.scala
@@ -0,0 +1,30 @@
+
+
+final class webSite$minusEvolution$percent20of$percent20Cinema$u002Eworksheet$_ {
+def args = webSite$minusEvolution$percent20of$percent20Cinema$u002Eworksheet_sc.args$
+def scriptPath = """webSite-Evolution%20of%20Cinema.worksheet.sc"""
+/*<script>*/
+
+/*</script>*/ /*<generated>*//*</generated>*/
+}
+
+object webSite$minusEvolution$percent20of$percent20Cinema$u002Eworksheet_sc {
+  private var args$opt0 = Option.empty[Array[String]]
+  def args$set(args: Array[String]): Unit = {
+    args$opt0 = Some(args)
+  }
+  def args$opt: Option[Array[String]] = args$opt0
+  def args$: Array[String] = args$opt.getOrElse {
+    sys.error("No arguments passed to this script")
+  }
+
+  lazy val script = new webSite$minusEvolution$percent20of$percent20Cinema$u002Eworksheet$_
+
+  def main(args: Array[String]): Unit = {
+    args$set(args)
+    val _ = script.hashCode() // hashCode to clear scalac warning about pure expression in statement position
+  }
+}
+
+export webSite$minusEvolution$percent20of$percent20Cinema$u002Eworksheet_sc.script as `webSite-Evolution%20of%20Cinema.worksheet`
+
